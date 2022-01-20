2c3ebf5e627f379f940b206480727c2f9d1f4e3b
tatsu513 <0285mashiko@gmail.com>
2022-01-21 00:35:14 +0900

    connect WPgraphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const API = 'https://countries.trevorblades.com';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
