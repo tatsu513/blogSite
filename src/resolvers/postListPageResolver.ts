f21eed3a4b62b95edb5924853654c17cd47c8770
tatsu513 <0285mashiko@gmail.com>
2022-02-08 22:19:29 +0900

    add log

--- src/resolvers/postListPageResolver.ts
+++ src/resolvers/postListPageResolver.ts
@@ -6,9 +6,13 @@ import getCategoriesByPosts from 'logics/getCategoriesByPosts';
 
 export const postListPageResolver = async (): Promise<ListPageResults> => {
   const graphqlSdk = getSdk(createGraphqlClient());
+  console.info({ graphqlSdk });
   const response = await graphqlSdk.homePage();
+  console.info({ response });
   const posts = WordpressPostsToPosts(response.posts.nodes);
+  console.info({ posts });
   const categories = getCategoriesByPosts(posts);
+  console.info({ categories });
 
   return { posts, categories };
 };
--- src/utils/createGraphqlClient.ts
+++ src/utils/createGraphqlClient.ts
@@ -1,6 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
 const createGraphqlClient = () => {
+  console.info({ v: String(process.env.NEXT_PUBLIC_WP_GQL) });
   return new GraphQLClient(String(process.env.NEXT_PUBLIC_WP_GQL));
 };
 
